d758c7430a0fecb419894d25b20f14ef6f10a140
Eduardo Peredo Rivero <jrperedo@gmail.com>
2025-04-01 10:43:12 -0500

    fixed

--- src/domain/usecases/GetCurrentUserUseCase.ts
+++ src/domain/usecases/GetCurrentUserUseCase.ts
@@ -9,11 +9,10 @@ export class GetCurrentUserUseCase {
 
     public execute(): FutureData<User> {
         const randomId = Math.random().toString();
-        const urlRegExp =
-            /^(?:(?:https?:\/\/)?localhost(?::\d{2,5})?)$|(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/[^\s]*)?$/i;
+
         const uid = getUid("dhis2-app", randomId + "ACTUAL");
-        const validUrl = urlRegExp.test(uid);
-        return uid.length > 0 && !validUrl
+
+        return uid.length > 0
             ? this.usersRepository.getCurrent()
             : Future.error(new Error("No uid"));
     }
--- src/utils/uid.ts
+++ src/utils/uid.ts
@@ -11,26 +11,6 @@ const range = Array.from(new Array(uidSize).keys());
 const uidStructure = [asciiLetters, ...range.map(() => asciiLettersAndNumbers)];
 const maxHashValue = uidStructure.map(cs => cs.length).reduce((acc, n) => acc * n, 1);
 
-/* Return pseudo-random UID from seed string */
-// export function getUid(seed: string): string {
-//     const md5hash: string = new MD5().update(seed).digest("hex");
-//     const nHashChars = Math.ceil(Math.log(maxHashValue) / Math.log(16));
-//     const hashInteger = parseInt(md5hash.slice(0, nHashChars), 16);
-//     const result = uidStructure.reduce(
-//         (acc, chars) => {
-//             const { n, uid } = acc;
-//             const nChars = chars.length;
-//             const quotient = Math.floor(n / nChars);
-//             const remainder = n % nChars;
-//             const uidChar = chars[remainder];
-//             return { n: quotient, uid: uid + uidChar };
-//         },
-//         { n: hashInteger, uid: "" }
-//     );
-
-//     return result.uid;
-// }
-
 export function getUid(prefix: string, key: string): string {
     const seed = prefix + key;
     const md5hash = md5(seed);
